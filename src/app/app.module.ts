382077488bd1920be7eb6189a7bc479f5a717ee9
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-09-24 17:05:52 -0400

    added components & views folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ComponentDocumentationComponent
   ],
   imports: [
     BrowserModule
