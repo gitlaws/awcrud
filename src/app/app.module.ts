ffb2c0983d2425b79ab663a2701d658049c3e95d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-09-24 17:09:10 -0400

    added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    ComponentDocumentationComponent
+    ComponentDocumentationComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
